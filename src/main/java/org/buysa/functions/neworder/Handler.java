4d0ad432e14fe6696099659aa5ee1ef95b5c3535
macsakini <macsakini@hotmail.com>
2023-02-14 21:45:18 +0300

    Functions

--- src/main/java/org/buysa/functions/neworder/Handler.java
+++ src/main/java/org/buysa/functions/neworder/Handler.java
@@ -6,14 +6,72 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.util.Map;
 
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 // Handler value: example.Handler
 public class Handler implements RequestHandler<Map<String,String>, String>{
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
     @Override
     public String handleRequest(Map<String,String> event, Context context)
     {
+
+        Properties localproperties = new Properties();
+        InputStream localfile = null;
+        try {
+            localfile= new FileInputStream("local.properties");
+            localproperties.load(localfile);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String Bootstrap_server = "13.246.19.88:9093";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, Bootstrap_server);
+        properties.setProperty(ProducerConfig.CLIENT_ID_CONFIG, "Buysa Lambda New Order");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "gzip");
+        properties.setProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/Users/mac/IdeaProjects/buysaeventsfunctions/private/client.truststore.jks");
+        properties.setProperty(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "SAkiniolinga@123");
+        properties.setProperty(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "/Users/mac/IdeaProjects/buysaeventsfunctions/private/server.keystore.jks");
+        properties.setProperty(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "SAkiniolinga@123");
+        properties.setProperty(SslConfigs.SSL_KEY_PASSWORD_CONFIG, "SAkiniolinga@123");
+
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String, String> message = new ProducerRecord<String, String>("orders", "hshsjsj", "jsjsjsj");
+
+        producer.send(message, (metadata, exception) -> {
+            Logger logger= LoggerFactory.getLogger(Main.class.getName());
+            if (exception == null) {
+                logger.info("Successfully received the details as: \n" +
+                        "Topic:" + metadata.topic() + "\n" +
+                        "Partition:" + metadata.partition() + "\n" +
+                        "Offset" + metadata.offset() + "\n" +
+                        "Timestamp" + metadata.timestamp());
+            }
+
+            else {
+                logger.error("Can't produce,getting error",exception);
+            }
+            // Log the info
+            logger.info("Message 1 for logger");
+        });
+
         LambdaLogger logger = context.getLogger();
         String response = new String("200 OK");
         // log execution details
@@ -22,6 +80,8 @@ public class Handler implements RequestHandler<Map<String,String>, String>{
         // process event
         logger.log("EVENT: " + gson.toJson(event));
         logger.log("EVENT TYPE: " + event.getClass().toString());
+//
+        producer.close();
         return response;
     }
 }
---
+++ src/main/java/org/buysa/functions/neworder/Main.java
@@ -0,0 +1,76 @@
+package org.buysa.functions.neworder;
+
+
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+
+public class Main{
+    private static Logger logger = Logger.getLogger(Main.class.getName());
+    static Slack slack = new Slack();
+
+    public static void main(String[] args){
+        logger.info("Logger Initialized");
+
+        InputStream propertiesfile = null;
+
+        Properties localproperties = new Properties();
+
+        try {
+            propertiesfile= new FileInputStream("local.properties");
+            localproperties.load(propertiesfile);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "13.246.19.88:9093");
+        properties.setProperty(ProducerConfig.CLIENT_ID_CONFIG, "Buysa Lambda New Order");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+//        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "gzip");
+//        properties.setProperty(SslConfigs.SSL_ENABLED_PROTOCOLS_CONFIG, "SSL");
+        properties.setProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, localproperties.getProperty("SSL_TRUSTSTORE_FILE"));
+        properties.setProperty(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, localproperties.getProperty("SSL_PASSWORD"));
+        properties.setProperty(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, localproperties.getProperty("SSL_KEYSTORE_FILE"));
+        properties.setProperty(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG,localproperties.getProperty("SSL_PASSWORD"));
+        properties.setProperty(SslConfigs.SSL_KEY_PASSWORD_CONFIG, localproperties.getProperty("SSL_PASSWORD"));
+
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String, String> message = new ProducerRecord<String, String>("items", "hshsjsj", "jsjsjsj");
+
+        try {
+            producer.send(message, (metadata, exception) -> {
+                if (exception == null) {
+                    logger.info("Successfully received the details as: \n" +
+                            "Topic:" + metadata.topic() + "\n" +
+                            "Partition:" + metadata.partition() + "\n" +
+                            "Offset" + metadata.offset() + "\n" +
+                            "Timestamp" + metadata.timestamp()
+                    );
+                }
+                else {
+                    logger.warning("Can't produce,getting error" + exception);
+                }
+            });
+            producer.flush();
+//            slack.slackSend("Template Message");
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        producer.close();
+    }
+}
